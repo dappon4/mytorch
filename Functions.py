13d298c20de84cb042a6ef0698002f238de73e28
Dahong Luo <dappon.04@gmail.com>
2024-06-13 23:31:08 +0900

    Conv2d implemented

--- Functions.py
+++ Functions.py
@@ -1,4 +1,4 @@
-import cupy as np
+import cupy as cp
 
 def identity(x):
     return x
@@ -7,7 +7,7 @@ def identity_derivative(x):
     return x
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1/(1+cp.exp(-x))
 
 def sigmoid_derivative(x):
     return x*(1-x)
@@ -15,32 +15,36 @@ def sigmoid_derivative(x):
 def softmax(x):
     # shape of x: (batch_size, 1, output_size)
     # calculate softmax for each row
-    x = np.exp(x - np.max(x))
-    return x / np.sum(x, axis=-1, keepdims=True)
+    x = cp.exp(x - cp.max(x))
+    return x / cp.sum(x, axis=-1, keepdims=True)
 
 def softmax_derivative(x):
     return x
 
-def relu(x):
-    return np.maximum(0, x)
-
-def relu_derivative(x):
-    return np.where(x > 0, 1, 0)
-
 def mean_squared_error(y_pred, y_true):
-    #print(np.mean(np.mean(np.power(y_pred - y_true,2),axis=-1)))
-    return np.mean(np.sum(np.power(y_pred - y_true,2),axis=-1)).item()
+    #print(cp.mean(cp.mean(cp.power(y_pred - y_true,2),axis=-1)))
+    return cp.mean(cp.sum(cp.power(y_pred - y_true,2),axis=-1)).item()
 
 def mean_squared_error_derivative(y_pred, y_true):
-    #print(np.mean(y_pred - y_true, axis = 0).shape)
+    #print(cp.mean(y_pred - y_true, axis = 0).shape)
     return 2 * (y_pred - y_true)
 
 def cross_entropy(y_pred, y_true):
     # cross entropy and softmax all together
     y_pred = softmax(y_pred)
-    return (-np.sum(y_true * np.log(y_pred)) / len(y_pred)).item()
+    return (-cp.sum(y_true * cp.log(y_pred)) / len(y_pred)).item()
 
 def cross_entropy_derivative(y_pred, y_true):
-    #print(np.mean(y_pred - y_true, axis=0))
+    #print(cp.mean(y_pred - y_true, axis=0))
     y_pred = softmax(y_pred)
     return y_pred - y_true
+
+def xavier_init(input_size, output_size):
+    return cp.random.normal(0.0, cp.sqrt(2/(input_size + output_size)), (input_size, output_size))
+
+def he_init_conv2d(filter_shape):
+    # filter shape (output_depth, input_depth, filter_height, filter_width)
+    fan_in = filter_shape[1] * filter_shape[2] * filter_shape[3] # number of input units, product of: input depth, filter height, filter width
+    stddev = cp.sqrt(2.0 / fan_in) # standard deviation of normal distribution
+
+    return cp.random.normal(loc=0, scale=stddev, size=filter_shape)
--- Layers.py
+++ Layers.py
@@ -1,5 +1,7 @@
 from typing import Any
+from Functions import *
 import cupy as cp
+import time
 
 class Layer:
     def __init__(self):
@@ -29,9 +31,6 @@ class Layer:
         self.training = False
         if self.prev:
             self.prev.eval()
-    
-    def xavier_init(self, input_size, output_size):
-        return cp.random.normal(0.0, cp.sqrt(2/(input_size + output_size)), (input_size, output_size))
 
 class CompoundLayer(Layer):
     def __init__(self):
@@ -68,7 +67,7 @@ class Linear(Layer):
     def __init__(self, input_size, output_size, lr = 0.005, dropout=0.0):
         super().__init__()
         self.lr = lr
-        self.weight = self.xavier_init(input_size, output_size)
+        self.weight = xavier_init(input_size, output_size)
         self.bias = cp.zeros((1, output_size))
         
         self.dropout = dropout
@@ -122,7 +121,77 @@ class Residual(Layer):
     def eval(self):
         for prev in self.prev:
             prev.eval()
+
+class Conv2d(Layer):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, lr=0.005):
+        super().__init__()
+        
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        
+        if type(kernel_size) == int:
+            self.kernel_size = (kernel_size, kernel_size)
+        else:
+            self.kernel_size = kernel_size
+        
+        if type(stride) == int:
+            self.stride = (stride, stride)
+        else:
+            self.stride = stride
+
+        self.padding = padding
+        self.lr = lr
+        
+        self.filter = he_init_conv2d((out_channels, in_channels, *self.kernel_size))
+        self.bias = None
     
+    def forward(self, x):
+        #x is cp array of shape (batch_size, in_channels, height, width)
+        # output cp array (batch size, out_channels, new_height, new_width)
+        #print((x.shape[0], self.out_channels, x.shape[2] - self.kernel_size[0] + 1, x.shape[3] - self.kernel_size[1] + 1))
+        batch_size = x.shape[0]
+        if self.bias is None:
+            self.bias = cp.zeros((1, self.out_channels, int((x.shape[2] - self.kernel_size[0] + 2 * self.padding)/self.stride[0] + 1), int((x.shape[3] - self.kernel_size[1] + 2 * self.padding)/self.stride[1] + 1)))
+        
+        out_height = int((x.shape[2] - self.kernel_size[0] + 2 * self.padding)/self.stride[0] + 1)
+        out_width = int((x.shape[3] - self.kernel_size[1] + 2 * self.padding)/self.stride[1] + 1)
+        
+        out = cp.zeros((batch_size, self.out_channels, out_height, out_width))
+        self.padded_input = cp.pad(x, ((0,0), (0,0), (self.padding, self.padding), (self.padding, self.padding)))
+        
+        for j in range(0, out_height): # height dimension
+            for k in range(0, out_width): # width dimension
+                start_height = j * self.stride[0]
+                start_width = k * self.stride[1]
+                cropped = self.padded_input[:, :, start_height:start_height+self.kernel_size[0], start_width:start_width+self.kernel_size[1]]
+                # cropped is shape (in channels, kernel height, kernel width)
+                out[:, :, j, k] = cp.tensordot(cropped, self.filter, axes=([1,2,3], [1,2,3]))
+
+        out += self.bias
+        
+        return out,self
+    
+    def backward(self, error):
+        # error is cp array of shape (batch_size, out_channels, new_height, new_width)
+        batch_size = error.shape[0]
+        
+        delta_filter = cp.zeros((batch_size,self.out_channels, self.in_channels, *self.kernel_size))
+        delta_error = cp.zeros((batch_size, self.in_channels, self.padded_input.shape[2], self.padded_input.shape[3]))
+        
+        for j in range(0, error.shape[2]): # height dimension
+            for k in range(0, error.shape[3]): # width dimension
+                delta_filter += error[:,:,j,k].reshape(batch_size,self.out_channels,1,1,1) * self.padded_input[:,:,j*self.stride[0]:j*self.stride[0]+self.kernel_size[0], k*self.stride[1]:k*self.stride[1]+self.kernel_size[1]].reshape(batch_size,1,self.in_channels,self.kernel_size[0],self.kernel_size[1])
+                delta_error[:,:,j*self.stride[0]:j*self.stride[0]+self.kernel_size[0],k*self.stride[1]:k*self.stride[1]+self.kernel_size[1]] += cp.sum(error[:,:,j,k].reshape(batch_size, self.out_channels,1,1,1) * self.filter, axis=1)
+    
+        self.filter -= self.lr * cp.mean(delta_filter, axis=0)
+        self.bias -= self.lr * cp.mean(error, axis=0)
+        
+        return delta_error[:,:,self.padding:-self.padding,self.padding:-self.padding]
+
+# TODO: Implement MaxPool2d
+# TODO: Implement Flatten
+# TODO: IMplement BatchNorm2d
+
 class Relu(Layer):
     def __init__(self):
         super().__init__()
@@ -135,3 +204,14 @@ class Relu(Layer):
     def backward(self, error):
         if self.prev:
             self.prev.backward(error * cp.where(self.input > 0, 1, 0))
+
+if __name__ == "__main__":
+    network = Conv2d(32, 64, (2,2), padding = 1)
+    
+    x = cp.random.randn(30,32,28,28)
+    x,_ = network.forward(x)
+    t1 = time.time()
+    x = network.backward(x)
+    t2 = time.time()
+    print(t2-t1)
+    print(x.shape)
