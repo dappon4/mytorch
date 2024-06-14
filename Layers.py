1a2b0edacf28bdc4eafc2b370e749550a5dc0716
Dahong Luo <dappon.04@gmail.com>
2024-06-14 14:40:14 +0900

    MaxPool2d implemented and some optimization for Conv2d

--- Layers.py
+++ Layers.py
@@ -1,6 +1,7 @@
 from typing import Any
 from Functions import *
 import cupy as cp
+from cupy.lib.stride_tricks import sliding_window_view
 import time
 
 class Layer:
@@ -146,9 +147,6 @@ class Conv2d(Layer):
         self.bias = None
     
     def forward(self, x):
-        #x is cp array of shape (batch_size, in_channels, height, width)
-        # output cp array (batch size, out_channels, new_height, new_width)
-        #print((x.shape[0], self.out_channels, x.shape[2] - self.kernel_size[0] + 1, x.shape[3] - self.kernel_size[1] + 1))
         batch_size = x.shape[0]
         if self.bias is None:
             self.bias = cp.zeros((1, self.out_channels, int((x.shape[2] - self.kernel_size[0] + 2 * self.padding)/self.stride[0] + 1), int((x.shape[3] - self.kernel_size[1] + 2 * self.padding)/self.stride[1] + 1)))
@@ -156,21 +154,15 @@ class Conv2d(Layer):
         out_height = int((x.shape[2] - self.kernel_size[0] + 2 * self.padding)/self.stride[0] + 1)
         out_width = int((x.shape[3] - self.kernel_size[1] + 2 * self.padding)/self.stride[1] + 1)
         
-        out = cp.zeros((batch_size, self.out_channels, out_height, out_width))
         self.padded_input = cp.pad(x, ((0,0), (0,0), (self.padding, self.padding), (self.padding, self.padding)))
+        sub_matrices = sliding_window_view(self.padded_input, (batch_size, self.in_channels, *self.kernel_size))
+        flattend = sub_matrices.reshape((-1, batch_size, self.in_channels, self.kernel_size[0]*self.kernel_size[1])).transpose(1,2,0,3)
         
-        for j in range(0, out_height): # height dimension
-            for k in range(0, out_width): # width dimension
-                start_height = j * self.stride[0]
-                start_width = k * self.stride[1]
-                cropped = self.padded_input[:, :, start_height:start_height+self.kernel_size[0], start_width:start_width+self.kernel_size[1]]
-                # cropped is shape (in channels, kernel height, kernel width)
-                out[:, :, j, k] = cp.tensordot(cropped, self.filter, axes=([1,2,3], [1,2,3]))
+        # flattend is shape (batch_size, in_channels, out_height*out_width, kernel_size[0]*kernel_size[1])
+        flattend_filter = self.filter.reshape((self.out_channels, self.in_channels, 1, self.kernel_size[0]*self.kernel_size[1]))
+        
+        return cp.tensordot(flattend, flattend_filter, axes=([1,3], [1,3])).transpose(0,2,1,3).reshape(batch_size, self.out_channels, out_height, out_width) + self.bias
 
-        out += self.bias
-        
-        return out,self
-    
     def backward(self, error):
         # error is cp array of shape (batch_size, out_channels, new_height, new_width)
         batch_size = error.shape[0]
@@ -188,7 +180,53 @@ class Conv2d(Layer):
         
         return delta_error[:,:,self.padding:-self.padding,self.padding:-self.padding]
 
-# TODO: Implement MaxPool2d
+class MaxPool2d(Layer):
+    def __init__(self,kernel_size, stride=1, padding=0):
+        super().__init__()
+        self.stride = stride
+        self.padding = padding
+        
+        if type(kernel_size) == int:
+            self.kernel_size = (kernel_size, kernel_size)
+        else:
+            self.kernel_size = kernel_size
+    
+    def forward(self,x):
+        batch_size, channel_size = x.shape[:2]
+        
+        output_height = int((x.shape[2] - self.kernel_size[0] + 2 * self.padding)/self.stride + 1)
+        output_width = int((x.shape[3] - self.kernel_size[1] + 2 * self.padding)/self.stride + 1)
+        
+        self.padded_input = cp.pad(x, ((0,0), (0,0), (self.padding, self.padding), (self.padding, self.padding)), mode="constant", constant_values = -10000)
+        sub_matrices = sliding_window_view(self.padded_input, (batch_size, channel_size, *self.kernel_size))
+        self.flattened = sub_matrices.reshape((-1, batch_size, channel_size, self.kernel_size[0]*self.kernel_size[1])).transpose(1,2,0,3)
+        
+        return cp.max(self.flattened, axis = -1).reshape(batch_size, channel_size, output_height, output_width)
+        
+    
+    def backward(self, error):
+        delta_error = cp.zeros(self.padded_input.shape)
+        
+        batch_size = error.shape[0]
+        channel_size = error.shape[1]
+        error_height = error.shape[2]
+        error_width = error.shape[3]
+        
+        max_values = cp.max(self.flattened, axis = -1)
+        max_values = cp.expand_dims(max_values, axis = -1)
+        bool_matrix = (self.flattened == max_values).astype(int)
+        
+        flattend_error = error.reshape(batch_size, channel_size, -1,1)
+        sub_matrix_error = bool_matrix * flattend_error
+        
+        sub_matrix_error = sub_matrix_error.reshape(batch_size, channel_size, error_height, error_width, self.kernel_size[0], self.kernel_size[1])
+        
+        for i in range(0, error_height):
+            for j in range(0, error_width):
+                delta_error[:,:,i*self.stride:i*self.stride+self.kernel_size[0],j*self.stride:j*self.stride+self.kernel_size[1]] += cp.sum(sub_matrix_error[:,:,i,j])
+        
+        return delta_error[:,:,self.padding:-self.padding,self.padding:-self.padding]
+
 # TODO: Implement Flatten
 # TODO: IMplement BatchNorm2d
 
@@ -206,12 +244,15 @@ class Relu(Layer):
             self.prev.backward(error * cp.where(self.input > 0, 1, 0))
 
 if __name__ == "__main__":
-    network = Conv2d(32, 64, (2,2), padding = 1)
+    network = MaxPool2d(kernel_size=(2,2), padding = 1)
+    x = cp.arange(32*5*2*2).reshape(32,5,2,2)
+    print(x)
     
-    x = cp.random.randn(30,32,28,28)
-    x,_ = network.forward(x)
+    y = network.forward(x)
     t1 = time.time()
-    x = network.backward(x)
+    z = network.backward(y)
     t2 = time.time()
     print(t2-t1)
-    print(x.shape)
+    #print(z.shape)
+    #print(y)
+    print(z.shape)
